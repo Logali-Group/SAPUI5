631d83e273822f6c4bc6673a5abaf80d4467e362
Logali-Group <training@logaligroup.com>
2020-03-26 13:38:36 +0000

    Commit 2

--- MyFirstProject/webapp/controller/View1.controller.js
+++ MyFirstProject/webapp/controller/View1.controller.js
@@ -6,6 +6,20 @@ sap.ui.define([
 	return Controller.extend("com.logaligroup.MyFirstProject.controller.View1", {
 		onInit: function () {
 
+		},
+
+		onValidate: function () {
+
+			var inputEmployee = this.byId("inputEmployee");
+			var valueEmployee = inputEmployee.getValue();
+            
+            //var varName = this.byId("inputEmployee").getValue().length;
+            
+			if (valueEmployee.length === 6) {
+				inputEmployee.setDescription("OK");
+			} else {
+				inputEmployee.setDescription("KO");
+			}
 		}
 	});
 });
